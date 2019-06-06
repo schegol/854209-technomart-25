bbca63fd197c9eb507055287ae345af1b61ac00a
schegol <48925789+schegol@users.noreply.github.com>
2019-06-07 00:41:37 +0300

    Исправления, добавление скрипта для модалок "Товар в корзине", настройка работоспособности попапов на обеих страницах

--- js/script.js
+++ js/script.js
@@ -1,70 +1,71 @@
-var openWriteUs = document.querySelector(".call-write-us");
 var popupWriteUs = document.querySelector(".popup-contact-us");
-var closeWriteUs = document.querySelector(".popup-close");
-var nameWriteUs = document.querySelector("[name=name]");
-var emailWriteUs = document.querySelector("[name=email]");
-var letterWriteUs = document.querySelector("[name=letter]");
-var formWriteUs = popupWriteUs.querySelector("form");
+if (popupWriteUs) {
+  var openWriteUs = document.querySelector(".call-write-us");
+  var closeWriteUs = popupWriteUs.querySelector(".popup-close");
+  var nameWriteUs = document.querySelector("[name=name]");
+  var emailWriteUs = document.querySelector("[name=email]");
+  var letterWriteUs = document.querySelector("[name=letter]");
+  var formWriteUs = popupWriteUs.querySelector("form");
 
-var isStorageSupport = true;
-var storage = "";
+  var isStorageSupport = true;
+  var storage = "";
 
-try {
-  storageName = localStorage.getItem("nameWriteUs");
-  storageEmail = localStorage.getItem("emailWriteUs");
-} catch (err) {
-  isStorageSupport = false;
+  try {
+    storageName = localStorage.getItem("nameWriteUs");
+    storageEmail = localStorage.getItem("emailWriteUs");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+  openWriteUs.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popupWriteUs.classList.add("popup-show");
+
+    if (storageName && storageEmail) {
+      nameWriteUs.value = storageName;
+      emailWriteUs.value = storageEmail;
+      letterWriteUs.focus();
+    } else {
+      nameWriteUs.focus();
+    }
+  });
+
+  closeWriteUs.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popupWriteUs.classList.remove("popup-show");
+    popupWriteUs.classList.remove("popup-error");
+  });
+
+  formWriteUs.addEventListener("submit", function (evt) {
+    if (!nameWriteUs.value || !emailWriteUs.value || !letterWriteUs.value) {
+      evt.preventDefault();
+      popupWriteUs.classList.remove("popup-error");
+      popupWriteUs.offsetWidth = popupWriteUs.offsetWidth;
+      popupWriteUs.classList.add("popup-error");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("nameWriteUs", nameWriteUs.value);
+        localStorage.setItem("emailWriteUs", emailWriteUs.value);
+      }
+    }
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popupWriteUs.classList.contains("popup-show")) {
+        popupWriteUs.classList.remove("popup-show");
+        popupWriteUs.classList.remove("popup-error");
+      }
+    }
+  });
 }
 
-openWriteUs.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popupWriteUs.classList.add("popup-show");
+///////////////////////////////////////////////////////////
 
-  if (storageName && storageEmail) {
-    nameWriteUs.value = storageName;
-    emailWriteUs.value = storageEmail;
-    letterWriteUs.focus();
-  } else {
-    nameWriteUs.focus();
-  }
-});
-
-closeWriteUs.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popupWriteUs.classList.remove("popup-show");
-  popupWriteUs.classList.remove("popup-error");
-});
-
-formWriteUs.addEventListener("submit", function (evt) {
-  if (!nameWriteUs.value || !emailWriteUs.value || !letterWriteUs.value) {
-    evt.preventDefault();
-    popupWriteUs.classList.remove("popup-error");
-    popupWriteUs.offsetWidth = popupWriteUs.offsetWidth;
-    popupWriteUs.classList.add("popup-error");
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem("nameWriteUs", nameWriteUs.value);
-      localStorage.setItem("emailWriteUs", emailWriteUs.value);
-    }
-  }
-});
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
-    if (popupWriteUs.classList.contains("popup-show")) {
-      popupWriteUs.classList.remove("popup-show");
-      popupWriteUs.classList.remove("popup-error");
-    }
-  }
-});
-
-
-// var openMap = document.querySelector(.call-map);
+// var openMap = document.querySelector("".call-map");
 // openMapPopup.classList.add(popup-show);
 //
-// var openItemAdded = document.querySelectorAll(.call-item-added);
-
 
 //
 // ymaps.ready(function () {
@@ -102,3 +103,35 @@ window.addEventListener("keydown", function (evt) {
 //     myMap.geoObjects
 //         .add(myPlacemarkWithContent);
 // });
+
+
+///////////////////////////////////////////////////////////
+
+var openItemAdded = document.querySelectorAll(".call-item-added");
+if (openItemAdded) {
+  var popupItemAdded = document.querySelector(".popup-item-added");
+  var closeItemAdded = popupItemAdded.querySelectorAll(".popup-close");
+
+  for (var i=0; i<openItemAdded.length; i++) {
+    openItemAdded[i].addEventListener("click", function (evt) {
+      evt.preventDefault();
+      popupItemAdded.classList.add("popup-show");
+    });
+  };
+
+  for (var n=0; n<closeItemAdded.length; n++) {
+    closeItemAdded[n].addEventListener("click", function (evt) {
+      evt.preventDefault();
+      popupItemAdded.classList.remove("popup-show");
+    });
+  };
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popupItemAdded.classList.contains("popup-show")) {
+        popupItemAdded.classList.remove("popup-show");
+      }
+    }
+  });
+}
